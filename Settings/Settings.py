b1c8bf27ac15da6b29368dd40e15ff434847e188
lavanblavan <lavanalvin127@gmail.com>
2025-10-05 20:23:27 +0530

    Update Extraction.py with new changes

--- Settings/Settings.py
+++ Settings/Settings.py
@@ -5,4 +5,6 @@ class Config:
     Groq_API_KEY = os.getenv("Groq_API_KEY")
     if Groq_API_KEY is None:
         raise ValueError("Groq_API_KEY not found in environment variables.")
+    documents_folder = r"C:\Users\Lavan\Desktop\Chatbot\Document_Summarizer\Documents"  # Folder containing document subfolders
+    
     # print(Groq_API_KEY)
